5dbdbbcb8b98dc1989bd1cc0a54a65bacae97724
пк <pk@MacBook-Pro-pk.local>
2019-06-28 20:53:20 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
 # 
-token = '741888612:AAFPlb66CSiMQbnN4EHrbXMnrxVb_hZkgHk'
+token = '741888612:AAFPlb66CSiMQbnN4EHrbXMnrxVb_hZkgHk' # @CRM_111bot
 
 couriers ={'156185969': 'Toronto', '777':'Miami' }
