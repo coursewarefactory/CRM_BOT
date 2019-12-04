389032cf62788caf2c12b25755b8b4dd20919d85
aquaracer <50531938+aquaracer@users.noreply.github.com>
2019-12-05 00:34:23 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,9 @@
 # -*- coding: utf-8 -*-
 # 
+import os
+
 token = '741888612:AAFPlb66CSiMQbnN4EHrbXMnrxVb_hZkgHk' # @CRM_111bot
+
+#token = os.environ.get('CRM_TOKEN')
 
 couriers ={'156185969': 'Toronto', '777':'Miami' }
